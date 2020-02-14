98422df6a82359c7962e99471c3aa2dd57559eb9
Norman <normanlambert455@gmail.com>
2020-02-13 20:31:14 -0500

    work in class today

---
+++ Assets/W2board.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class W2board : MonoBehaviour
+{
+    public GameObject player;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            player.transform.position += new Vector3(0, 0, 1);
+
+
+
+        }
+    }
+}
